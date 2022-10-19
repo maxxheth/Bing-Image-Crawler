d5e3b81b92f10722eba669f1a9af5ea1751c16cb
= <=>
2022-10-18 23:53:44 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+// For more information, see https://crawlee.dev/
+import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { router } from './routes.js';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const headless = process.env.USE_BROWSER === 'yes' || !!process.env.USE_BROWSER
+
+console.log({headless})
+
+// throw Error('PAUSE!!')
+
+const startUrls = ['https://bing.com/images/feed'];
+
+const crawler = new PlaywrightCrawler({
+    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    requestHandler: router,
+    headless
+    // maxRequestRetries: 200,
+});
+
+await crawler.run(startUrls);
---
+++ src/routes.ts
@@ -0,0 +1,105 @@
+import { Dataset, createPlaywrightRouter, EnqueueStrategy } from 'crawlee';
+
+export const router = createPlaywrightRouter();
+
+import fs from 'fs'
+
+import https from 'https'
+
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const download = (url: string) => {
+    
+    const path = process.env.FILEPATH as string
+    
+    try {
+
+        fs.mkdir(path, err => err)
+
+    } catch (error) {
+
+
+    } finally {
+
+        const splitUrlFrags = url.split('/')
+
+        const lastFrag = splitUrlFrags[splitUrlFrags.length - 1]
+
+        https.get(url, (res) => {
+            const writeStream = fs.createWriteStream(path + lastFrag);
+
+            res.pipe(writeStream);
+
+            writeStream.on("finish", () => {
+                writeStream.close();
+                console.log("Download Completed!");
+            })
+        })
+
+    }
+
+
+}
+
+router.addDefaultHandler(async ({ enqueueLinks, page, log }) => {
+
+    log.info('Kicking off initial scraping task...')
+
+    await page.waitForSelector('.b_searchbox')
+
+    await page.locator('.b_searchbox').fill(process.env.KEYWORD as string)
+
+    await page.locator('.b_searchboxSubmit').click()
+
+    await page.waitForTimeout(200000)
+
+    const imgUrls = await page.$$eval('a.iusc', (links: HTMLAnchorElement[]) => {
+
+        return links.map(link => link.href)        
+
+    })
+
+    await enqueueLinks({
+        globs: imgUrls, 
+        label: 'CAROUSEL'
+    })
+
+    
+
+});
+
+router.addHandler('CAROUSEL', async ({ page, log }) => {
+    
+    log.info('Carousel started')
+
+    await page.waitForSelector('.mainImage.current .overlayContainer')
+
+    try {
+
+        const mainImageSrc = await page.$eval('.mainImage.current .overlayContainer img', (imgElem: HTMLImageElement) => {
+
+            return imgElem.src
+
+        })
+
+        download(mainImageSrc)
+
+    } catch (error) {
+
+        console.log({error})
+
+    } finally {
+
+        /**
+         * 
+         * Silence is golden. 
+         * 
+         **/       
+
+    }
+
+    await page.waitForTimeout(3000)
+
+})
